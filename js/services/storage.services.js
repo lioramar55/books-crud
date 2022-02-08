d27d2d5b5845f19cdc7373bd119125f369cbe5cc
lioramar55 <lioramar55@gmail.com>
2022-02-08 12:41:40 +0200

    fix things

--- js/services/storage.services.js
+++ js/services/storage.services.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 function saveToStorage(key, val) {
@@ -10,16 +9,3 @@ function loadFromStorage(key) {
   const str = localStorage.getItem(key);
   return JSON.parse(str);
 }
-=======
-'use strict';
-
-function saveToStorage(key, val) {
-  const str = JSON.stringify(val);
-  localStorage.setItem(key, str);
-}
-
-function loadFromStorage(key) {
-  const str = localStorage.getItem(key);
-  return JSON.parse(str);
-}
->>>>>>> 97f87707a9da920f27e2415fb7c9356e9794344c
